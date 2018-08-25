ec9ffb6184228d30e2da4910805aa99c935e99fb
Denis Isaev <sr_relikt@mail.ru>
2018-08-25 23:24:03 +0300

    Lesson 11 - Promise task v3

--- lesson-11-promise/js/script.js
+++ lesson-11-promise/js/script.js
@@ -126,18 +126,26 @@ window.addEventListener('DOMContentLoaded', function() {
 	}
  
 	function mask(event) {
-		let matrix = "+7 (___) ___-__-__",
+		var matrix = "+7 (___) ___-__-__",
 				i = 0,
 				def = matrix.replace(/\D/g, ""),
 				val = this.value.replace(/\D/g, "");
 		if (def.length >= val.length) val = def;
-		this.value = matrix.replace(/./g, function(a) {
-		return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+		this.value = matrix.replace(/[_\d]/g, function(a) {
+		    return  i < val.length ? val.charAt(i++) :  a
 		});
+		i = this.value.indexOf("_");
+		if(val.length < len) i = this.value.lastIndexOf(val.substr(-1))+1;
+		if (i != -1) {
+		i < 5 && (i = 3);
+		this.value = this.value.slice(0,i);
+		}
 		if (event.type == "blur") {
-			if (this.value.length == 2) this.value = "";
-	  } else setCursorPosition(this.value.length, this);
+			if (this.value.length < 5) this.value = ""
+		} else setCursorPosition(this.value.length, this);
+		len = val.length;
 	}
+
 	let input = document.querySelector("#tel");
 	input.addEventListener("input", mask, false);
 	input.addEventListener("focus", mask, false);
