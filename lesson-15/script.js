d9b4e484ea2cbc25267134ed4ed0f2528402ecd1
Denis Isaev <sr_relikt@mail.ru>
2018-09-02 19:10:06 +0300

    Lesson 15 - Simple Task

---
+++ lesson-15/script.js
@@ -0,0 +1,24 @@
+// Функция sum должна возвращать тип данных true. Проверить её на это.
+		function sum(a, b) {
+		return a + b > 10;
+		}
+		sum(2,2);
+
+
+
+		// Переменная num должна быть равна 5. Проверить на соответствие.
+		let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+		let num = arr[1][1];
+
+		// Узнать, что нам вернет функция each в данных условиях. Проверить её на тип данных, который она возвращает,
+		// на соответствие ожидаемому результату (который вы получили) и на свойство length.
+		var each = function(startArr, f){return f(startArr)};
+		var newArr = [64, 49, 36, 25, 16];
+		var myFunc = function(a){
+			var newArr = [];
+			for (var i = 0; i < a.length; i++) {
+				newArr[i]=Math.sqrt(a[i]);
+			}
+			return newArr;
+		}
+		console.log(each(newArr, myFunc));
---
+++ lesson-15/test.js
@@ -0,0 +1,26 @@
+describe('Проверка на истинность', function() {
+	
+	it('Функция sum возвращает тип данных true?', function() {
+		assert.equal(sum(), true);
+	});
+});
+
+describe('Проверка на истинность', function(){
+	it('Проверка num == 5', function() {
+		assert.equal(num, 5);
+	});
+});
+
+describe('Функция each', function(){
+	it('Возвращаемое значение соответствует типу данных array', function() {
+		assert.typeOf(each(newArr, myFunc), 'array');
+	});
+
+	it('Проверка newArr.length == 5', function() {
+		assert.lengthOf(each(newArr, myFunc), 5);
+	});
+
+	it('Проверка результат each(newArr, myFunc) == [8, 7, 6, 5, 4]', function() {
+		assert.equal(each(newArr, myFunc).sort().join(',') == [8, 7, 6, 5, 4].sort().join(','), true);
+	});
+});
