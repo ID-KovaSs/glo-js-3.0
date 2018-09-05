3f75760cb1fa62df83ced62ff24de8b62eeff289
Denis Isaev <sr_relikt@mail.ru>
2018-09-05 19:42:37 +0300

    Lesson 17 - Simple task v1

---
+++ lesson-17/js/clock.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function setDate() {  
+  var hours = document.querySelector('.hours');
+  var minutes = document.querySelector('.minutes');
+  var seconds = document.querySelector('.seconds');
+  
+  var month = document.querySelector('.month');
+  var day = document.querySelector('.day');
+  var year = document.querySelector('.year');
+  var now = new Date();
+	var mm = now.getMonth();
+	var dd = now.getDate();
+	var yyyy = now.getFullYear();
+	var secs = now.getSeconds();
+	var mins = now.getMinutes();
+	var hrs = now.getHours();
+	var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+	hours.innerHTML = hrs;
+	
+	if (secs < 10) {
+		seconds.innerHTML = '0' + secs;
+	} else {
+		seconds.innerHTML = secs;
+	}
+
+	if (mins < 10) {
+		minutes.innerHTML = '0' + mins;
+	} else {
+		minutes.innerHTML = mins;
+  }
+
+	month.innerHTML = monthName[mm];
+	day.innerHTML = dd;
+	year.innerHTML = yyyy;
+}
+
+setInterval(setDate, 1000);
