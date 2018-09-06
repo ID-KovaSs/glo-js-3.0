06d3fa9dbc7d4541193acdd5d6a01fdefbb55d15
Denis Isaev <sr_relikt@mail.ru>
2018-09-06 20:17:26 +0300

    Modul-3 v1

--- GraduationProject/js/bundle/bundle.js
+++ GraduationProject/js/bundle/bundle.js
@@ -50,23 +50,41 @@ module.exports = headerSlider;
 },{}],3:[function(require,module,exports){
 function popupDesign() {
 
-  let popupBtn = document.querySelectorAll('.button-design'),
-      popupDesign = document.querySelector('.popup-design'),
-      closeBtn = document.querySelectorAll('.popup-close')[2];
-      body = document.querySelector('body');
-      console.log(popupBtn);
+  let popupDesign = document.querySelector('.popup-design'),
+      popupConsult = document.querySelector('.popup-consultation'),
+      closeBtn = document.querySelectorAll('.popup-close')[2],
+      body = document.querySelector('body'),
+      buttonDesign = false,
+      buttonConsult = false;
 
   // Делегирование событий кросбраузерного всплытия модального окна на кнопках с классом "button-design"
   body.addEventListener('click', function(e) {
-    let target = e.target;
+    let target = e.target,
+        mobileAgent = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+        IEAgent = /MSIE 10/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent);
+
     if(target.classList.contains("button-design") && target.classList.contains("button-order")) {
-      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        console.log(target);
-        popupDesign.style.display = "block";
-        document.body.style.overflow = "hidden";
-        } else if(/MSIE 10/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+      if( mobileAgent ) {
+        buttonDesign = true;
+        animationAppIn();
+        } else if(IEAgent) {
+          buttonDesign = true;
           animationAppIn();
           } else {
+            buttonDesign = true;
+            animationPcIn(this);
+          }
+    }
+
+    if(target.classList.contains("button-consultation") && target.classList.contains("button-order")) {
+      if( mobileAgent ) {
+        buttonConsult = true;
+        animationAppIn();
+        } else if(IEAgent) {
+          buttonConsult = true;
+          animationAppIn();
+          } else {
+            buttonConsult = true;
             animationPcIn(this);
           }
     }
@@ -74,20 +92,35 @@ function popupDesign() {
 
 // Анимация для десктопных браузеров
 function animationPcIn(param) {
-  popupDesign.style.display = "block";
-  document.body.style.overflow = "hidden";
+  if(buttonDesign) {
+    console.log("десктопное сособытие");
+    popupDesign.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
+  if(buttonConsult) {
+    console.log("десктопное сособытие");
+    popupConsult.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
 
 }
 // Для мобильных приложений
 function animationAppIn() {
-  // popupDesign.classList.add('fadeIn');
+  console.log("мобильное сособытие");
+  popupDesign.classList.add('fadeIn');
   popupDesign.style.display = "block";
   document.body.style.overflow = "hidden";	
 }
 
-closeBtn.addEventListener('click', () => {
-  popupDesign.style.display = "none";
-  document.body.style.overflow = "";	
+body.addEventListener('click', function(e) {
+  let target = e.target;
+  if(target.classList.contains("popup-close")){
+    buttonDesign = false;
+    buttonConsult = false;
+    popupDesign.style.display = "none";
+    popupConsult.style.display = "none";
+    document.body.style.overflow = "";	
+  }
 });
 }
 
